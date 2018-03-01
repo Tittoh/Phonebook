7b6007b881ad34d0d98cf547e3ead7d0ef3c6832
Tittoh <tittohb@gmail.com>
2018-03-01 21:09:27 +0300

    debug all tests, clean-up tab and space conflicts

--- test_contact.py
+++ test_contact.py
@@ -3,25 +3,21 @@ import unittest
 from contact import Contact
 
 class TestContact(unittest.TestCase):
-    def test_add_contact(self):
-        contact = Contact()
-        response = contact.add_contact("Tittoh", "0729261228")
-        self.assertEqual(response["message"], "Contact added successfully")
+	def setUp(self):
+		self.contact = Contact()
+
+	def test_add_contact(self):
+		result = self.contact.add_contact("Tittoh", "0729261228")
+		self.assertEqual(result["message"], "Contact added successfully")
 
 	def test_update_contact(self):
-		contact = Contact()
-		response = contact.update_contact({"Tittoh", "0729261228"},{"Tittoh", "0736197716"})
-		self.assertEqual(response["message"], "Contact updated successfully")
+		result =self.contact.update_contact("Tittoh", "0729261228", "0736197716")
+		self.assertEqual(result["message"], "Contact updated successfully")
 
 	def test_delete_contact(self):
-		contact = Contact()
-		response = contact.delete_contact("Ken", "0734123456")
-		self.assertEqual(response["message"], "Contact deleted successfully")
+		result = self.contact.delete_contact("Ken", "0734123456")
+		self.assertEqual(result["message"], "Contact deleted successfully")
 
 	def test_view_contact(self):
-		contact = Contact()
-		response = contact.view_contact()
-		self.assertEqual(response["message"], "All contacts displayed")
-
-if __name__ == '__main__':
-	unittest.main()
+		result = self.contact.view_contact("Emmy", "099345667")
+		self.assertEqual(result, "Contact for Emmy is 099345667")
